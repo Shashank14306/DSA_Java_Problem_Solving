c4610cde27bfb7338874b8e5af770d3a93aa55ff
Shashank Priyadarshi <shashank.kvs2019@gmail.com>
2026-03-25 02:15:15 +0530

    StringCompression

---
+++ src/Strings_In_Java/StringCompression.java
@@ -0,0 +1,24 @@
+package Strings_In_Java;
+
+public class StringCompression {
+    public static void main(String[] args) {
+//        int i = 0 , index = 0;
+//        while(i < chars.length) {
+//            char current = chars[i];
+//            int count = 0;
+//            while(i < chars.length && chars[i] == current) {
+//                i++;
+//                count++;
+//            }
+//            chars[index++] = current;
+//            if(count > 1) {
+//                String str = String.valueOf(count);
+//                for(char c : str.toCharArray()) {
+//                    chars[index++] = c;
+//                }
+//            }
+//        }
+//
+//        return index;
+    }
+}
