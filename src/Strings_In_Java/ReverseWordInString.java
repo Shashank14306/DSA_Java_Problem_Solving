9b4fd3f03cee355273a6cceaedda2663a0dee262
Shashank Priyadarshi <shashank.kvs2019@gmail.com>
2026-03-26 19:21:23 +0530

    string

---
+++ src/Strings_In_Java/ReverseWordInString.java
@@ -0,0 +1,15 @@
+package Strings_In_Java;
+
+public class ReverseWordInString {
+    public static void main(String[] args) {
+//        String[] words = s.trim().split("\\s+");
+//        StringBuilder res = new StringBuilder();
+//
+//        for(int i = words.length - 1; i >= 0; i--) {
+//            res.append(words[i]).append(" ");
+//        }
+//
+//        return res.toString().trim();
+    }
+
+}
