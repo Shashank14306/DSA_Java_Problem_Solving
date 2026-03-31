0e1ee3ba6dc622abd4a4fa10888800283c7f80ff
Shashank Priyadarshi <shashank.kvs2019@gmail.com>
2026-03-31 22:58:07 +0530

    golbalvariables

--- src/Recursion_In_Java/GlobalVariables.java
+++ src/Recursion_In_Java/GlobalVariables.java
@@ -1,12 +1,13 @@
 package Recursion_In_Java;
 
 public class GlobalVariables {
+    static int x =17;
     public static void main(String[] args) {
-        int x =10;
-        fun(x);
+//        int x =10;
+        fun();
         System.out.println(x);
     }
-    public static void fun(int x){
+    public static void fun(){
         x=20;
     }
 }
