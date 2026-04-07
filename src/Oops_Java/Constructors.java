aaffeb81d08d0ee2e34401ef0145aed08a19b701
Shashank Priyadarshi <shashank.kvs2019@gmail.com>
2026-04-08 02:13:15 +0530

    constructors in java

---
+++ src/Oops_Java/Constructors.java
@@ -0,0 +1,12 @@
+package Oops_Java;
+
+public class Constructors {
+    public static class Car{
+        int seats;
+        String name;
+        double length;
+    }
+    public static void main(String[] args) {
+
+    }
+}
