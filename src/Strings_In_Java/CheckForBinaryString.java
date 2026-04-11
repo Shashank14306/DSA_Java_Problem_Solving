4ff4db1d145ea0dc20cf496fb739233686d9643a
Shashank Priyadarshi <shashank.kvs2019@gmail.com>
2026-04-11 22:41:04 +0530

    check for binary string

---
+++ src/Strings_In_Java/CheckForBinaryString.java
@@ -0,0 +1,18 @@
+package Strings_In_Java;
+
+public class CheckForBinaryString {
+    public static void main(String[] args) {
+        class Solution {
+            boolean isBinary(String s) {
+                for (int i = 0; i < s.length(); i++) {
+                    char ch = s.charAt(i);
+
+                    if (ch != '0' && ch != '1') {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
