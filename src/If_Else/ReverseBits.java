395a8dda1302b24e40691538c1356169aeb5f9cd
Shashank Priyadarshi <shashank.kvs2019@gmail.com>
2026-04-15 22:51:25 +0530

    reverse bits

---
+++ src/If_Else/ReverseBits.java
@@ -0,0 +1,19 @@
+package If_Else;
+
+public class ReverseBits {
+    public static void main(String[] args) {
+        class Solution {
+            public int reverseBits(int n) {
+                int result = 0;
+
+                for (int i = 0; i < 32; i++) {
+                    int lastBit = n & 1;
+                    result = (result << 1) | lastBit;
+                    n = n >> 1;
+                }
+
+                return result;
+            }
+        }
+    }
+}
