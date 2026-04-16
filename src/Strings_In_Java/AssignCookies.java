6970417511c7175b5cd8759b776caec43616dcbd
Shashank Priyadarshi <shashank.kvs2019@gmail.com>
2026-04-16 22:48:02 +0530

    assign cookies

---
+++ src/Strings_In_Java/AssignCookies.java
@@ -0,0 +1,21 @@
+package Strings_In_Java;
+import java.util.*;
+public class AssignCookies {
+    public static void main(String[] args) {
+        class Solution {
+            public int findContentChildren(int[] g, int[] s) {
+                Arrays.sort(g);
+                Arrays.sort(s);
+                int i = 0;
+                int j = 0;
+                while (i < g.length && j < s.length) {
+                    if (s[j] >= g[i]) {
+                        i++;
+                    }
+                    j++;
+                }
+                return i;
+            }
+        }
+    }
+}
