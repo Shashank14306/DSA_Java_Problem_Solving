49ff9da60ade4b1c214d7d1107a244d914ffa2e5
Shashank Priyadarshi <shashank.kvs2019@gmail.com>
2026-04-20 22:51:58 +0530

    unique path 2

--- src/Linked_List_Java/DisplayList.java
+++ src/Linked_List_Java/DisplayList.java
@@ -1,4 +1,7 @@
 package Linked_List_Java;
+
+
+
 class node{
     int val;
     Node next;
@@ -7,8 +10,18 @@ class node{
     }
 }
 public class DisplayList {
+    public static void displayRec(Node head){
+        if(head == null) return;
+        System.out.print(head.val+" ");
+        displayRec(head.next);
+    }
     public static void display(Node head){
-
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.val+" ");
+            temp = temp.next;
+        }
+        System.out.println();
     }
     public static void main(String[] args) {
         Node a = new Node(10);
@@ -16,7 +29,7 @@ public class DisplayList {
         Node c = new Node(30);
         Node d = new Node(40);
         Node e = new Node(50);
-        Node f = new Node(50);
+//        Node f = new Node(5);
 
         // Connect Kareneg ( Link )
 
@@ -24,7 +37,18 @@ public class DisplayList {
         b.next = c;
         c.next = d;
         d.next = e;
-        e.next = f;
-        display(a);
+//        e.next = f;
+//        display(a);
+//        displayRec(a);
+
+        System.out.println(get(a, 2));
+    }
+
+    private static int get(Node head , int idx){
+        Node temp = head;
+        for(int i=1; i<=idx; i++){
+            temp = temp.next;
+        }
+        return temp.val;
     }
 }
