71160835a3dfe5a0286952ecd48b1ef763e966df
Shashank Priyadarshi <shashank.kvs2019@gmail.com>
2026-04-24 23:23:11 +0530

    sum of natural number

---
+++ src/If_Else/SumOfNaturalNumber.java
@@ -0,0 +1,13 @@
+package If_Else;
+
+public class SumOfNaturalNumber {
+    class Solution {
+        static int sumOfNaturals(int n) {
+            int sum = 0;
+            for(int i = 1; i <= n; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
