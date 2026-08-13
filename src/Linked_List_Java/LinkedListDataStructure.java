8c1d09a1157123041bb8b784bcaafac2a05face3
Shashank Priyadarshi <shashank.kvs2019@gmail.com>
2026-08-13 19:27:55 +0530

    linked list

--- src/Linked_List_Java/LinkedListDataStructure.java
+++ src/Linked_List_Java/LinkedListDataStructure.java
@@ -19,24 +19,59 @@ class Linkedlist {
         }
     }
 
-     void display() {
-        if(head == null) return;;
-        Node temp = head;
-        while (temp != null){
-            System.out.print(temp.val+" ");
-            temp = temp.next;
+    void deleteAtHead() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
         }
-         System.out.println();
-    }
-}
-public class LinkedListDataStructure {
-    public static void main(String[] args) {
-        Linkedlist ll = new Linkedlist();
-        ll.addAtTail(10);
-        ll.addAtTail(20);
-        ll.addAtTail(30);
-        ll.addAtTail(40);
-        ll.display();
+        head = head.next;
+        if (head == null) tail = null;
     }
 
+    void display() {
+        if (head == null) return;
+        ;
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    //     void delete(int idx) {
+//        if(idx<0 || idx>=size){
+//            System.out.println("Invalid index!");
+//            return;
+//        }
+//        if(idx==0){
+//            deleteAtHead();
+//            return;
+//        }
+//        Node temp = head;
+//        int size;
+//        for(int i=1; i<=idx-1; i++){
+//            temp = temp.next;
+//        }
+//        temp.next = temp.next.next;// delete
+//         if(idx == size-1) tail = temp;
+//         size--;
+//
+//    }
+//}
+    public class LinkedListDataStructure {
+        public static void main(String[] args) {
+            Linkedlist ll = new Linkedlist();
+            ll.addAtTail(10);
+            ll.addAtTail(20);
+            ll.addAtTail(30);
+            ll.addAtTail(40);
+            ll.display();
+            ll.deleteAtHead();
+            ll.display();
+
+//        ll.delete(2); ll.display();
+        }
+
+    }
 }
